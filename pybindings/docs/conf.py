7182d28acd825e1941e4f65deb16bf8c8ba939c0
Aleks Kissinger <aleks0@gmail.com>
2025-06-26 15:03:20 +0100

    version push

--- pybindings/docs/conf.py
+++ pybindings/docs/conf.py
@@ -11,7 +11,7 @@ import bitgauss
 project = 'bitgauss'
 copyright = '2025, Aleks Kissinger'
 author = 'Aleks Kissinger'
-release = '0.3.1'
+release = '0.3.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
